032f9e08b275ecadd2c5b5bbff82e08fa24a1fe0
Scott MInor <scott77598@outlook.com>
2024-09-20 22:52:34 -0500

    Am I working?

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import StarRating from "./StarRating.js";
+import "./index.css";
+import App from "./App.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
